a60186e24f6fecdde328292335241fae2488793f
Balazs-D <balazs.danyadi@gmail.com>
2020-02-19 15:44:49 +0100

    toggle work 100%, cursor: pointer added

--- office-management/src/components/Employees.js
+++ office-management/src/components/Employees.js
@@ -1,29 +1,40 @@
 import React, { useContext } from 'react';
-import OfficeContext from '../context/officeContext'
+import OfficeContext from '../context/officeContext';
 
 const Employees = () => {
-
   const officeContext = useContext(OfficeContext);
-
 
   return (
     <div className='w-25'>
       <ul className='list-group'>
         {officeContext.staff.map((employee, i) => {
-
-          
           return (
             <li
               key={i}
               className='list-group-item border border-info d-flex justify-content-between'
             >
               {employee.name}
-              <span className='d-flex w-50 p-2 badge badge-secondary justify-content-between' id={i} onClick={officeContext.isHere}>
+              <span
+                className='d-flex w-50 p-2 badge badge-secondary justify-content-between'
+                id={i}
+                onClick={officeContext.isHere}
+                style={{cursor: 'pointer'}}
+              >
                 {employee.dep}
                 {employee.available ? (
-                  <p className='badge badge-info'>available</p>
+                  <p
+                    onClick={officeContext.blockClick}
+                    className='badge badge-info'
+                  >
+                    available
+                  </p>
                 ) : (
-                  <p className='badge badge-danger'>away</p>
+                  <p
+                    onClick={officeContext.blockClick}
+                    className='badge badge-danger'
+                  >
+                    away
+                  </p>
                 )}
               </span>
             </li>
@@ -34,4 +45,4 @@ const Employees = () => {
   );
 };
 
-export default Employees
+export default Employees;
--- office-management/src/context/OfficeState.js
+++ office-management/src/context/OfficeState.js
@@ -7,40 +7,61 @@ const OfficeState = props => {
   const initialState = {
     week: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
     staff: [
-      { name: 'Alex', available: false, dep: 'Tech' },
+      {
+        name: 'Alex',
+        available: false,
+        dep: 'Tech'
+      },
       { name: 'Bea', available: true, dep: 'PM' },
       { name: 'Carl', available: true, dep: 'HR' },
-      { name: 'Dasha', available: true, dep: 'B-Dev' },
-      { name: 'Eric', available: true, dep: 'Buss-Dev' },
-      { name: 'Francesca', available: true, dep: 'F-Dev' },
-      { name: 'Gabriella', available: true, dep: 'OM' }
+      {
+        name: 'Dasha',
+        available: true,
+        dep: 'B-Dev'
+      },
+      {
+        name: 'Eric',
+        available: true,
+        dep: 'Buss-Dev'
+      },
+      {
+        name: 'Francesca',
+        available: true,
+        dep: 'F-Dev'
+      },
+      {
+        name: 'Gabriella',
+        available: true,
+        dep: 'OM'
+      }
     ]
   };
 
   const [state, dispatch] = useReducer(OfficeReducer, initialState);
 
-  // Additional Functions
+  // Toggle available
 
-  const isHere = (e) => {
-
-
-    const index = e.target.id
+  const isHere = e => {
+    const index = e.target.id;
     const status = state.staff[index].available;
-    console.log('1: ' + status)
+    console.log('1: ' + status);
 
     let updatedStaff = { ...state };
     updatedStaff.staff[index].available = !status;
-    
-    
 
     dispatch({
-        type: IS_HERE,
-        payload: updatedStaff
-    })
-  
-   console.log(updatedStaff)
-    console.log('2: ' + status);
+      type: IS_HERE,
+      payload: updatedStaff
+    });
 
+    console.log(updatedStaff);
+    console.log('2: ' + status);
+  };
+
+  // Block child click
+
+  const blockClick = e => {
+    e.stopPropagation();
   };
 
   return (
@@ -48,7 +69,8 @@ const OfficeState = props => {
       value={{
         week: state.week,
         staff: state.staff,
-        isHere
+        isHere,
+        blockClick
       }}
     >
       {props.children}
